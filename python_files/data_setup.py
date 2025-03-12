a1fb18ba9f281e816c66fe37a6db51fecc6c8a33
kkodweis2013 <kkodweis@buffalo.edu>
2025-03-12 16:36:30 -0400

    updated data_setup.py to include the DataLoader needed for this application

--- python_files/data_setup.py
+++ python_files/data_setup.py
@@ -3,11 +3,54 @@ import csv
 import requests
 import pandas as pd
 
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
-#csv_file = "RAW_DATA/mymoviedb_EAS510.csv"
+
 ROOT_FOLDER = ""
+NUM_WORKERS = os.cpu_count()
 
-#data = pd.read_csv(csv_file) #created a Pandas Dataframe object
+def create_dataloaders(train_dir: str, test_dir: str, transform: transforms.Compose, batch_size: int, num_workers = NUM_WORKERS):
+
+    """ Creates training and testing DataLoaders.
+        
+        Takes in a training directory and testing directory path and turns
+        them into PyTorch Datasets and then into PyTorch DataLoaders.
+        
+        Args:
+            train_dir: Path to training directory.
+            test_dir: Path to testing directory.
+            transform: torchvision transforms to perform on training and testing data.
+            batch_size: Number of samples per batch in each of the DataLoaders.
+            num_workers: An integer for number of workers per DataLoader.
+
+        Returns:
+            A tuple of (train_dataloader, test_dataloader, class_names).
+            Where class_names is a list of the target classes.
+
+        Example usage:
+            train_dataloader, test_dataloader, class_names = \
+                = create_dataloaders(train_dir=path/to/train_dir,
+                                    test_dir=path/to/test_dir,
+                                    transform=some_transform,
+                                    batch_size=32,
+                                    num_workers=4)
+
+        If we'd like to make DataLoader 's we can now use the function within data_setup.py like so:
+        
+        Let's put our TinyVGG() model class into a script, note sometimes people just name it models.py and put all their baseline models (three or four) in one file.
+"""
+    train_data = datasets.ImageFolder(train_dir, transform = transform.)
+    test_data = datasets.ImageFolder(test_dir, transform = transform)
+
+    classes = train_data.classes
+
+    train_dataloader = DataLoader(train_data, batch_size, shuffle=True, num_workers = NUM_WORKERS, pin_memory= True)
+
+    test_dataloader = DataLoader(test_data, batch_size, shuffle=True, num_workers = NUM_WORKERS, pin_memory= True)
+
+    return train_dataloader, test_dataloader, classes
+
 
 def set_root_path(path):
 
@@ -246,3 +289,12 @@ def ld_img_dir(path, label_array):
 
     print(f"Full sample image set download complete")
     
+
+def main():
+    # Your code here
+    train_dataloader, test_dataloader, class_names = create_dataloaders("datasets/SampleV0/Train", "datasets/SampleV0/Test", batch_size=32)
+
+    print(class_names)
+
+if __name__ == "__main__":
+    main()
